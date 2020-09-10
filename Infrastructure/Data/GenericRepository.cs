ffd3731203c010413919c7124a49eef8b0db3382
Mehdi Kiani <mehdi.kiani@gmail.com>
2020-09-11 00:33:28 +0430

    End of section 4

---
+++ Infrastructure/Data/GenericRepository.cs
@@ -0,0 +1,45 @@
+using System.Linq;
+using Core.Entities;
+using Core.Interfaces;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Core.Specifications;
+
+namespace Infrastructure.Data
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly StoreContext _context;
+        public GenericRepository(StoreContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<T> GetByIdAdync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetEntityWithSpec(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).FirstOrDefaultAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).ToListAsync();
+        }
+
+        private IQueryable<T> ApplySpecification(ISpecification<T> spec)
+        {
+            return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
+        }
+    }
+}
